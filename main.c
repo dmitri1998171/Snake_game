955b280afaf51fb170510b16adc9c5b8071661b4
dmitri1998171 <dmitri199817@gmail.com>
2022-09-11 18:24:11 +0700

    draw the snake over the map

--- main.c
+++ main.c
@@ -2,23 +2,14 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <pthread.h>
+#include <termios.h>
  
 #define WIDTH 20
 #define HEIGHT 10
 
+short loop = 1;
 char map[HEIGHT][WIDTH];
-/*char map[HEIGHT][WIDTH] =  {
-    "00000000000000000000",
-    "0                  0",
-    "0                  0",
-    "0                  0",
-    "0                  0",
-    "0                  0",
-    "0                  0",
-    "0                  0",
-    "0                  0",
-    "00000000000000000000"
-};*/
 
 void createMap() {
     for (int i = 0; i < HEIGHT; i++) {
@@ -41,12 +32,21 @@ void drawMap() {
     }
 }
 
-void getKeyInput() {
-    int key = getc(stdin);
-        
+void* getKeyInput(void *args) {
+    pthread_detach(pthread_self());
+
+    // Отключаем канонический режим ввода для stdin (отключаем нажатие Enter)
+    struct termios term;
+    tcgetattr(STDIN_FILENO, &term);
+    term.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, 0, &term);
+    
+    while(loop) {
+        int key = getc(stdin);
+            
         switch (key) {
             case 119:   // w
-                
+                printf(" world\n");
                 break;
             
             case 115:   // s
@@ -62,8 +62,11 @@ void getKeyInput() {
                 break;
 
             case 27:    // ESC
-                exit(0);
+                loop = 0;
         }
+    }
+
+    return 0;
 }
 
 int main(void) {
@@ -72,22 +75,27 @@ int main(void) {
 
     createMap();
     
-/*
-*/
-    while(1) {
+    pthread_t thread;
+    int status;
+    int status_addr;
+ 
+    status = pthread_create(&thread, NULL, getKeyInput, NULL);
+
+    while(loop) {
         drawMap();
 
         for (int i = 0; i < snakeSize; i++) {
             map[HEIGHT / 2][(WIDTH / 2) + i] = snake[i];
         }
         
-        getKeyInput();
-        
         usleep(300);
 
         printf("\033[2J");      // Очистить терминал
         printf("\033[0;0f");    // Перевести каретку в левое верхнее положение
     }
+
+/*
+*/
 
     return 0;
 }
